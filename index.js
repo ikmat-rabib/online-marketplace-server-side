ac38dc160474f305d9796a2b4c048d109c640765
ikmat-rabib <ikmat.rbb@gmail.com>
2023-11-14 02:53:33 +0600

    created api for deleting jobs

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/jobs/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await jobCollection.deleteOne(query)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
